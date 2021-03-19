192c334d973987eb4abb2fa8aaf9739dee5b688e
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2021-03-19 23:16:41 +0000

    Start footer component

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer>
+      Built by{" "}
+      <a href="https://gattuso.dev" target="_blank" rel="noopener noreferrer">
+        <img
+          src="../static/logo.svg"
+          alt="Gattuso Logo"
+          width="34"
+          height="34"
+        />{" "}
+      </a>
+    </footer>
+  );
+}
