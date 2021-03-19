1ad282df2bac8ebb9a9bf3cfe46d6a51a1aa3662
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2021-03-19 23:16:17 +0000

    Add profile search form to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,85 @@
+import React, { useState } from "react";
+import Router from "next/router";
+import styled from "styled-components";
+import { MarkGithubIcon } from "@primer/octicons-react";
 import Head from "../components/Head";
+import { theme, mixins } from "../styles";
+const { colors, fonts } = theme;
+
+const StyledContainer = styled.div`
+  ${mixins.flexCenter};
+  background: ${colors.black};
+  background-image: linear-gradient(
+    ${colors.black} 0%,
+    ${colors.darkGrey} 100%
+  );
+  color: ${colors.offWhite};
+  height: 100vh;
+`;
+
+const StyledForm = styled.form`
+  background: transparent;
+  border-radius: 5px;
+  padding: 2rem;
+  margin-bottom: 20vh;
+  max-width: 600px;
+  text-align: center;
+`;
+
+const StyledIcon = styled(MarkGithubIcon)`
+  color: ${colors.blue};
+`;
+
+const StyledLabel = styled.label`
+  display: block;
+  font-size: 2.5rem;
+  font-weight: 500;
+  margin: 2rem;
+`;
+
+const StyledInput = styled.input`
+  background: #26303c;
+  border-radius: 0.25rem;
+  border: 0;
+  color: ${colors.lightblue};
+  font-family: ${fonts.mono};
+  font-size: 2rem;
+  font-weight: 400;
+  margin: 0 auto;
+  padding: 1rem;
+  outline: 0;
+  text-align: center;
+  width: 100%;
+  max-width: 500px;
+`;
 
 export default function Home() {
-  return (
-    <>
-      <Head title="GitHub Profiles" />
-      <main>
-        <h1>GitHub Profiles</h1>
-      </main>
+  const [username, setUsername] = useState("");
+  const handleChange = (e) => setUsername(e.target.value);
 
-      <footer>
-        Built by{" "}
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  return (
+    <main>
+      <Head title="GitHub Profiles" />
+      <StyledContainer>
+        <StyledForm
+          onSubmit={(e) => {
+            e.preventDefault();
+            Router.push({
+              pathname: "/user",
+              query: { id: username },
+            });
+          }}
         >
-          <img
-            src="../static/logo.svg"
-            alt="Gattuso Logo"
-            width="34"
-            height="34"
-          />{" "}
-        </a>
-      </footer>
-    </>
+          <StyledIcon size="large" />
+          <StyledLabel htmlFor="username">Find Your GitHub Profile</StyledLabel>
+          <StyledInput
+            name="username"
+            type="text"
+            placeholder="username"
+            onChange={handleChange}
+          />
+        </StyledForm>
+      </StyledContainer>
+    </main>
   );
 }
