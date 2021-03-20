e6fe4c46de310e92f2de4a9595ac9bf415c224f6
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2021-03-20 01:23:13 +0000

    Update global styles

--- styles/GlobalStyle.js
+++ styles/GlobalStyle.js
@@ -257,31 +257,94 @@ const GlobalStyle = createGlobalStyle`
     font-style: italic;
   }
 
-html {
-  box-sizing: border-box;
-}
+  html {
+    box-sizing: border-box;
+  }
 
-*,
-*:before,
-*:after {
-  box-sizing: inherit;
-}
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
 
-::selection {
-  background: ${colors.blue};
-  color: ${colors.white};
-}
+  ::selection {
+    background-color: ${colors.blue};
+    color: ${colors.white};
+  }
 
-body {
-  background: ${colors.offWhite};
-  color: ${colors.darkGrey};
-  font-family: ${fonts.inter};
-  margin: 0;
-  padding: 0;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+  body {
+    margin: 0;
+    padding: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-family: ${fonts.inter};
+    color: ${colors.darkGrey};
+    background-color: ${colors.offWhite};
+  }
 
+  h1, h2, h3, h4, h5, h6 {
+    margin-top: 0;
+    margin-bottom: 1rem;
+    font-weight: 500;
+  }
+
+  p {
+    line-height: 1.5;
+  }
+
+  a {
+    text-decoration: none;
+    transition: ${theme.transition};
+    color: ${colors.blue};
+    display: inline-block;
+
+    &:hover,
+    &:focus {
+    }
+  }
+
+  ul, ol {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+  }
+
+  img {
+    width: 100%;
+    max-width: 100%;
+    vertical-align: middle;
+  }
+
+  svg {
+    fill: currentColor;
+  }
+
+  button {
+    cursor: pointer;
+    border: 0;
+    border-radius: 0;
+    transition: ${theme.transition};
+    &:focus,
+    &:active {
+      outline: 0;
+    }
+  }
+
+  input {
+    border-radius: 0;
+    outline: 0;
+    &:focus {
+      outline: 0;
+    }
+    &::placeholder {
+    }
+    &:focus,
+    &:active {
+      &::placeholder {
+        opacity: 0.5;
+      }
+    }
+  }
 `;
 
 export default GlobalStyle;
