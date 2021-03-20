a2ecf0e5f506515acfd7dfc97a90df4af5fda753
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2021-03-20 01:29:02 +0000

    Update user info component

--- components/UserInfo.js
+++ components/UserInfo.js
@@ -1,5 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {
+  BriefcaseIcon,
+  CalendarIcon,
+  LocationIcon,
+} from "@primer/octicons-react";
 import Octicon, { Briefcase, Calendar, Location } from "@primer/octicons-react";
 import { Section } from "../styles";
 
@@ -14,7 +19,46 @@ const UserInfo = ({ userData }) => (
         )}
 
         {userData.name && <h1>{userData.name}</h1>}
-      </div>
+
+        {userData.login && (
+          <h2>
+            <a
+              href={userData.html_url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @{userData.login}
+            </a>
+          </h2>
+        )}
+
+        <div className="info">
+          {userData.company && (
+            <span className="info__item">
+              <BriefcaseIcon size="small" />
+              {userData.company}
+            </span>
+          )}
+
+          {userData.location && (
+            <span className="info__item">
+              <LocationIcon size="small" />
+              {userData.location}
+            </span>
+          )}
+
+          {userData.created_at && (
+            <span className="info__item">
+              <CalendarIcon size="small" />
+              Joined{" "}
+              {new Date(userData.created_at).toLocaleDateString("en-US", {
+                month: "long",
+                day: "numeric",
+                year: "numeric",
+              })}
+            </span>
+          )}
+        </div>
     )}
   </Section>
 );
