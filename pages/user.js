1b9bda5c0a5bffaaf2f04f203665ec203feaf795
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2021-03-20 20:39:05 +0000

    Remove extra styles

--- pages/user.js
+++ pages/user.js
@@ -1,27 +1,9 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
 import { Head, UserInfo, Repos } from "../components";
 import GhPolyglot from "gh-polyglot";
 import { theme, mixins } from "../styles";
 const { colors, fonts } = theme;
-
-const StyledContainer = styled.div`
-  ${mixins.flexCenter};
-  background: ${colors.black};
-  background-image: linear-gradient(
-    ${colors.black} 0%,
-    ${colors.darkGrey} 100%
-  );
-  color: ${colors.offWhite};
-  height: 100vh;
-`;
-
-const StyledTitle = styled.h1`
-  font-size: 3rem;
-  color: ${colors.lightblue};
-  font-weight: 400;
-`;
 
 export default function User(props) {
   const username = props.query.id;
