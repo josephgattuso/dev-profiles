a9e9bb3055d742e4e06dffb94d10006f4bffc3b5
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2021-03-21 12:18:15 +0000

    Add charts component

--- components/index.js
+++ components/index.js
@@ -1,6 +1,7 @@
 import Head from "./Head";
 import Corner from "./Corner";
 import UserInfo from "./UserInfo";
+import Charts from "./Charts";
 import Repos from "./Repo";
 
-export { Head, Corner, UserInfo, Repos };
+export { Head, Corner, UserInfo, Charts, Repos };
